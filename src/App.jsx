d080bdb5a543d606f0cd9cb4b831292464a60781
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2025-01-15 22:45:07 +0700

    feat: integrate deepch converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import $ from "jquery";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const suuid = "MY_STREAM";
+  const stream = new MediaStream();
+  const videoElem = useRef(null);
+  const divElem = useRef(null);
+
+  const config = {
+    iceServers: [
+      {
+        urls: ["stun:stun.l.google.com:19302"],
+      },
+    ],
+  };
+
+  const pc = new RTCPeerConnection(config);
+
+  const log = (msg) => {
+    if (divElem.current) {
+      divElem.current.innerHTML += msg + "<br>";
+    }
+  };
+
+  const handleNegotiationNeededEvent = async () => {
+    let offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    getRemoteSdp();
+  };
+
+  const getCodecInfo = () => {
+    $.get(`http://localhost:8083/stream/codec/${suuid}`, function (data) {
+      try {
+        data = JSON.parse(data);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        $.each(data, function (index, value) {
+          pc.addTransceiver(value.Type, {
+            direction: "sendrecv",
+          });
+        });
+      }
+    });
+  };
+
+  const getRemoteSdp = () => {
+    $.post(
+      `http://localhost:8083/stream/receiver/${suuid}`,
+      {
+        suuid: suuid,
+        data: btoa(pc.localDescription.sdp),
+      },
+      function (data) {
+        try {
+          pc.setRemoteDescription(
+            new RTCSessionDescription({
+              type: "answer",
+              sdp: atob(data),
+            })
+          );
+        } catch (e) {
+          console.warn(e);
+        }
+      }
+    );
+  };
+
+  useEffect(() => {
+    $("#" + suuid).addClass("active");
+    getCodecInfo();
+
+    pc.onnegotiationneeded = handleNegotiationNeededEvent;
+
+    pc.ontrack = function (event) {
+      stream.addTrack(event.track);
+      if (videoElem.current) {
+        videoElem.current.srcObject = stream;
+      }
+      log(event.streams.length + " track is delivered");
+    };
+
+    pc.oniceconnectionstatechange = (e) => log(pc.iceConnectionState);
+  }, [suuid, stream]);
 
   return (
-    <>
+    <div>
+      <div ref={divElem}></div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="col">
+          <input type="hidden" name="suuid" id="suuid" value="MY_STREAM" />
+          {/* <input type="hidden" name="port" id="port" value=":8083" /> */}
+          <input type="hidden" id="localSessionDescription" readOnly={true} />
+          <input type="hidden" id="remoteSessionDescription" />
+          <div id="remoteVideos">
+            <video
+              style={{ width: "600px" }}
+              id="videoElem"
+              ref={videoElem}
+              autoPlay
+              muted
+              controls
+            ></video>
+          </div>
+          <div id="div"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
