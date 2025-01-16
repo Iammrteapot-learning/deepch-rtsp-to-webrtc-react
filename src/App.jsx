b873ba3d8a82ae405151b8e0a45420876e6fd221
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2025-01-16 14:21:49 +0700

    refactor: phase 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,17 @@
-import { useEffect, useRef } from "react";
-import $ from "jquery";
+import LocalVideo from "./components/LocalVideo";
 
-const App = () => {
-  const stream_server = "http://localhost:8083";
-  const suuid = "MY_STREAM";
-  const stream = new MediaStream();
-  const videoElem = useRef(null);
-
-  useEffect(() => {
-    const config = {
-      iceServers: [
-        {
-          urls: ["stun:stun.l.google.com:19302"],
-        },
-      ],
-    };
-
-    const pc = new RTCPeerConnection(config);
-
-    const log = (msg) => {
-      console.log(msg);
-    };
-
-    const handleNegotiationNeededEvent = async () => {
-      let offer = await pc.createOffer();
-      await pc.setLocalDescription(offer);
-      getRemoteSdp();
-    };
-
-    const getCodecInfo = () => {
-      $.get(`${stream_server}/stream/codec/${suuid}`, function (data) {
-        try {
-          data = JSON.parse(data);
-        } catch (e) {
-          console.log(e);
-        } finally {
-          $.each(data, function (index, value) {
-            pc.addTransceiver(value.Type, {
-              direction: "sendrecv",
-            });
-          });
-        }
-      });
-    };
-
-    const getRemoteSdp = () => {
-      $.post(
-        `${stream_server}/stream/receiver/${suuid}`,
-        {
-          suuid: suuid,
-          data: btoa(pc.localDescription.sdp),
-        },
-        function (data) {
-          try {
-            pc.setRemoteDescription(
-              new RTCSessionDescription({
-                type: "answer",
-                sdp: atob(data),
-              })
-            );
-          } catch (e) {
-            console.warn(e);
-          }
-        }
-      );
-    };
-
-    getCodecInfo();
-
-    pc.onnegotiationneeded = handleNegotiationNeededEvent;
-
-    pc.ontrack = function (event) {
-      stream.addTrack(event.track);
-      if (videoElem.current) {
-        videoElem.current.srcObject = stream;
-      }
-      log(event.streams.length + " track is delivered");
-    };
-
-    pc.oniceconnectionstatechange = (e) => log(pc.iceConnectionState);
-  }, [suuid, stream]);
-
+function App() {
   return (
-    <video
-      style={{ width: "600px" }}
-      id="videoElem"
-      ref={videoElem}
-      autoPlay
-      muted
-      controls
-    ></video>
+    <div>
+      <div>Below is the LocalVideo Component</div>
+      <br />
+      <div>
+        We use deepch/RTSPtoWebRTC to convert source RTSP video to native
+        browser WebRTC
+      </div>
+      <LocalVideo stream_server={"http://localhost:8083"} suuid={"MY_STREAM"} />
+    </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/LocalVideo.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useRef } from "react";
+import $ from "jquery";
+
+const LocalVideo = ({ stream_server, suuid }) => {
+  const stream = new MediaStream();
+  const videoElem = useRef(null);
+
+  useEffect(() => {
+    const config = {
+      iceServers: [
+        {
+          urls: ["stun:stun.l.google.com:19302"],
+        },
+      ],
+    };
+
+    const pc = new RTCPeerConnection(config);
+
+    const log = (msg) => {
+      console.log(msg);
+    };
+
+    const handleNegotiationNeededEvent = async () => {
+      let offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      getRemoteSdp();
+    };
+
+    const getCodecInfo = () => {
+      $.get(`${stream_server}/stream/codec/${suuid}`, function (data) {
+        try {
+          data = JSON.parse(data);
+        } catch (e) {
+          console.log(e);
+        } finally {
+          $.each(data, function (index, value) {
+            pc.addTransceiver(value.Type, {
+              direction: "sendrecv",
+            });
+          });
+        }
+      });
+    };
+
+    const getRemoteSdp = () => {
+      $.post(
+        `${stream_server}/stream/receiver/${suuid}`,
+        {
+          suuid: suuid,
+          data: btoa(pc.localDescription.sdp),
+        },
+        function (data) {
+          try {
+            pc.setRemoteDescription(
+              new RTCSessionDescription({
+                type: "answer",
+                sdp: atob(data),
+              })
+            );
+          } catch (e) {
+            console.warn(e);
+          }
+        }
+      );
+    };
+
+    getCodecInfo();
+
+    pc.onnegotiationneeded = handleNegotiationNeededEvent;
+
+    pc.ontrack = function (event) {
+      stream.addTrack(event.track);
+      if (videoElem.current) {
+        videoElem.current.srcObject = stream;
+      }
+      log(event.streams.length + " track is delivered");
+    };
+
+    pc.oniceconnectionstatechange = (e) => log(pc.iceConnectionState);
+  }, [suuid, stream]);
+
+  return (
+    <video
+      style={{ width: "600px" }}
+      id="videoElem"
+      ref={videoElem}
+      autoPlay
+      muted
+      controls
+    ></video>
+  );
+};
+
+export default LocalVideo;
